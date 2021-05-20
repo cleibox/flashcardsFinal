2a53478432bb859d29c8afd5e800ade0acd78596
CLbox <51213177+cleibox@users.noreply.github.com>
2021-05-20 11:24:32 -0400

    base code

---
+++ flashcardsCode.java
@@ -0,0 +1,11 @@
+/**
+ * Date: May 20, 2021
+ * Name: Cynthia Lei, Daiphy Lee, Johnny He, Sophia Nguyen
+ * Teacher: Mr. Ho
+ * Description: Flashcards Final Project
+ */
+ 
+ class flashcardsCode {
+    public static void main(String[] args) {
+    }
+ }
