6608e4ab905c93d2013f1a83acb8f1c4e5a83a1f
CLbox <cleibox@gmail.com>
2021-05-23 21:46:27 -0400

    read txt lines

--- flashcardsCode.java
+++ flashcardsCode.java
@@ -5,9 +5,36 @@
  * Description: Flashcards Final Project
  */
  
-// test
+// Imports
+import java.util.Scanner; // scanner
+import java.io.IOException;
+import java.io.*;
+import java.io.File; 
 
- class flashcardsCode {
-    public static void main(String[] args) {
-    }
- }
+class flashcardsCode {
+   public static void main(String[] args){
+      String path = "/Users/cynthia/Documents/CompSci11/summatives/flashcardsFinal/";
+      String name = "test.txt";
+
+      readTxtFile(path, name);
+   }
+
+    // Reads the file
+    public static void readTxtFile(String filePath, String fileName){
+       String line = "";
+       try{ 
+         File file = new File(filePath+fileName);
+         BufferedReader br = new BufferedReader(new FileReader(fileName));
+         while ((line = br.readLine()) != null){
+            System.out.println(line);
+         }
+
+       }
+       // Program cannot find file
+       catch (IOException e){
+          System.out.println("Invalid");
+       } 
+
+  }
+
+}
