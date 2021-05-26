c0e1a6229188ed4b8ef6c62394197009adf644f5
daiphy <349857383@gapps.yrdsb.ca>
2021-05-26 10:23:09 -0400

    reads csv file and determines questions and answers

--- flashcardsCode.java
+++ flashcardsCode.java
@@ -1,116 +1,81 @@
+
 /**
  * Date: May 20, 2021
  * Name: Cynthia Lei, Daiphy Lee, Johnny He, Sophia Nguyen
  * Teacher: Mr. Ho
  * Description: Flashcards Final Project
  */
-
-// Imports
+ 
+//import classes
 import java.util.Scanner; // scanner
+// File imports
 import java.io.IOException;
 import java.io.*;
 import java.io.File;
-import java.util.ArrayList; // arrayList
+// CSV reader
+// Get the included jar file in the github
+// In VSCode, Explorer > JAVA PROJECTS > Referenced Libraries > Add library (the jar file)
+// import org.apache.commons.csv.CSVFormat;
+// import org.apache.commons.csv.CSVParser;
+// import org.apache.commons.csv.CSVRecord;
+import com.opencsv.CSVReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-class flashcardsCode {
-   public static void main(String[] args) {
-      // OBV I SHALL DELETE THIS WHEN MERGING WITH OTHERS
-      ArrayList<String> questionsArrList = new ArrayList<String>(); 
-      ArrayList<String> answersArrList = new ArrayList<String>(); 
-      
-      // System.out.println("Enter file path: ");
-      // String path = reader.nextLine();
-      String path = "/Users/cynthia/Documents/CompSci11/summatives/flashcardsFinal/";
+ class flashcardsCodet {
+    public static void main(String[] args) {
 
-      // System.out.println("Enter file name: ");
-      // String name = reader.nextLine();
-      String name = "test.txt";
+      String path = "/Users/daiphylee/11 ComSci Java Lessons/A. Assignments/flashcardsFinal/";
+      String name = "tester.csv";
+      String fullroute = path+name;
+      // int sizefornow = 100;
+      String[] questionsArr = new String[totalLinesInFile(fullroute)];
+      String[] answersArr = new String[totalLinesInFile(fullroute)];
+      String[] linesArr = new String[totalLinesInFile(fullroute)];
 
-      readTxtFile(path, name, questionsArrList, answersArrList);
+      readCSVFile(fullroute, questionsArr, answersArr, linesArr);
+
+   }
+   public static void readCSVFile(String route, String[] questionArr, String[] answerArr, String[] lineArr){
+      String line = "";
+      String realQuestion = "";
+
+      // ArrayList<String> allArr = new ArrayList<String>();
+      try{
+            // Reader reader = Files.newBufferedReader(Paths.get(route));
+            // CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+            CSVReader reader = new CSVReader(new FileReader(route));
+
+
+            while ((lineArr = reader.readNext()) != null) {
+               // // answerArr = lineArr[findAnswers(totalLinesInFile(route))];
+               // answerArr = (lineArr[findAnswers(totalLinesInFile(route))]);
+               System.out.println("Question: " + lineArr[findQuestion(totalLinesInFile(route))]);
+               // System.out.println("Answer: ");
+               // printArr(lineArr[findQuestion(totalLinesInFile(route))]);
+           }
+         
+         // csvParser.close();
+         // CSVReader.close();
+      }
+      catch(Exception e){
+         System.out.println("Error occured. Please reinput.");
+         
+     }
    }
 
-   // /**
-   //  * @author Cynthia Lei Determine the total number of lines in the given file.
-   //  *         This is so we can initialize arrays the size of the total file lines
-   //  * 
-   //  * @param filePath user inputted file's path to access the file
-   //  * @param fileName user inputted file's name to access the file
-   //  * @return the number of lines in the file
-   //  */
-   // public static int totalLinesInFile(String filePath, String fileName) {
-   //    int totalLines = 0;
-   //    try {
-   //       String line = "";
-   //       File file = new File(filePath + fileName);
-   //       BufferedReader br = new BufferedReader(new FileReader(file));
-   //       while ((line = br.readLine()) != null) {
-   //          // if (!line.equals("")){
-   //          //    totalLines++; 
-   //          //    // this is if we want to discard blank flashcards so we'd only count lines with either a question or answer
-   //          // }
-   //          totalLines++;
-   //       }
-   //       br.close();
-   //    }
-
-   //    // Program cannot find file
-   //    catch (IOException e) {
-   //       System.out.println("Invalid file");
-   //    }
-   //    return totalLines;
-   // }
-
-   /**
-    * @author Cynthia Lei
-    * Reads the .txt file by identifying the question and answer portion of each line.
-    * Then it will sort the question and answer strings into their respective arraylists 
-    * 
-    * @param filePath user inputted .txt file path
-    * @param fileName user inputted .txt file name
-    * @param questionsArrayList arraylist that contains all the question strings
-    * @param answersArrayList arraylist that contains all the answer strings
-    */
-   public static void readTxtFile(String filePath, String fileName, ArrayList<String> questionsArrayList, ArrayList<String> answersArrayList) {
-      String txtLine = " ";
-      int questionMarkIndex = 0;
-      String realQuestion = "";
-      String realAnswer = "";
-
+   //cynthias method 
+   public static int totalLinesInFile(String fullfilePath) {
+      int totalLines = 0;
       try {
-         File file = new File(filePath + fileName);
+         String line = "";
+         File file = new File(fullfilePath);
          BufferedReader br = new BufferedReader(new FileReader(file));
-         while ((txtLine = br.readLine()) != null) {
-            // we assume the user's txt file is formatted with 1 question, denotated
-            // by a '?' and followed by the answer ALL ON THE SAME LINE
-            // in addition, there should be a space after the '?'
-            // Example:
-            // What is your name? Mr. Ho
-            System.out.println("line is <" + txtLine + ">");
-
-            // Dividing the line into 2 parts: question and answer
-            questionMarkIndex = findQuestionMarkTxt(txtLine); 
-            
-            // Adding the question portion into the question array
-            realQuestion = getQuestion(txtLine, questionMarkIndex);
-            troubleshootAndAddElementToArr(questionsArrayList, realQuestion);
-            
-            // Adding the answer portion into the answers array
-            realAnswer = getAnswer(txtLine, questionMarkIndex);
-            troubleshootAndAddElementToArr(answersArrayList, realAnswer);
-
-            // if (!((questionsArray[lineNum].equals("Placeholder")) && (answersArray[lineNum].equals("Placeholder")))) {
-            //    lineNum++; // no need to save the elements if both of them are placeholder. it's equivalent to a blank flashcard.
-            // }
-            
-            System.out.println();
-
+         while ((line = br.readLine()) != null) {
+            totalLines++;
          }
-         System.out.println("QUESTIONS arr: ");
-         printArrList(questionsArrayList);
-         System.out.println("ANSWERS arr: ");
-         printArrList(answersArrayList);
-         // System.out.println("Total line count: " + totalLinesInFile(filePath, fileName));
-
          br.close();
       }
 
@@ -118,100 +83,35 @@ class flashcardsCode {
       catch (IOException e) {
          System.out.println("Invalid file");
       }
+      return totalLines;
    }
-
-   /**
-    * @author Cynthia Lei
-    * Stores the question as a string
-    *
-    * @param txtLine this line from the .txt will be read
-    * @param questionMarkLoc the index of the question mark in the line
-    * @return the question portion in the line as a string
-    */
-   public static String getQuestion(String txtLine, int questionMarkLoc){
-      // The question starts from the beginning of each line to the '?'
-      // the questionMarkLoc goes up to but does not include the '?'
-      // so we need to add 1 to include the '?' in the question string
-      
-      // if there is no question, questionMarkLoc + 1 = 0 so the question would be ""
-      // that is important for troubleshooting (whether a placeholder is necessary)
-      String question = txtLine.substring(0, questionMarkLoc + 1);
-      System.out.println("question is !" + question);
-      return question;
-   }
-
-   /**
-    * @author Cynthia Lei
-    * Stores the answer as a string
-    * 
-    * @param txtLine this line from the .txt will be read
-    * @param questionMarkLoc the index of the question mark in the line
-    * @return the answer portion in the line as a string
-    */
-   public static String getAnswer(String txtLine, int questionMarkLoc){
-      String answer = "";
-      try{
-         // We assume the answer is followed by a space after the '?' so we add 2 
-         // End at the line length since the answer goes all the way to the end
-         answer = txtLine.substring(questionMarkLoc + 2, txtLine.length());
-      }
-      // No answer so (questionMarkLoc + 2) will be out of bounds
-      catch (java.lang.StringIndexOutOfBoundsException e) { 
-         answer = ""; // later we will use this string value to troubleshoot
-      }
-      
-      // No question. Answer is at the start of line
-      if (questionMarkLoc == -1){ 
-         answer = txtLine.substring(0, txtLine.length());
-      }
-      
-      System.out.println("answer is !" + answer);
-      return answer;
-   }
-
-   /**
-    * @author Cynthia Lei
-    * Determining whether it is necessary to add a placeholder. 
-    * Usually that is when there is no question or answer in the line
-    * 
-    * @param arrList either the questions arraylist or the answers arraylist
-    * @param element the question or answer string that is to be added into the questions or answers array respectively
-    */
-   public static void troubleshootAndAddElementToArr(ArrayList<String> arrList, String element){
-      if (element.equals("")){ // no question or answer present in the line
-         // this prevents an array from "lagging behind" due to missing elements
-         arrList.add("Placeholder"); 
-      }
-      else {
-         arrList.add(element);
-      }
-   }
-   
-   /**
-    * @author Cynthia Lei Determine the location of the question the txt line
-    * 
-    * @param txtLine Given a line of a txt file
-    * @return the index of the question mark to locate the end of a question;
-    *         otherwise return 0 if the user messes up and there is no question in
-    *         the line
-    */
-   public static int findQuestionMarkTxt(String txtLine) {
-      int len = txtLine.length();
-      for (int i = 0; i < len; i++) {
-         // search for '?'
-         if (txtLine.charAt(i) == '?') {
+   public static int findQuestion(int countLines){
+      for (int i = 0; i < countLines; i++){
+         // search for every other line
+         if (i % 2 == 0) {
             return i;
          }
+         
       }
-      return -1; // no '?' which means no question
+      return -1;
    }
-
-   public static void printArrList(ArrayList<String> arrList) {
-      for (int i = 0; i < arrList.size(); i++) {
-         System.out.print(arrList.get(i) + ",");
+   public static int findAnswers(int countLines){
+      for (int i = 0; i < countLines; i++){
+         // search for every other line
+         if (i % 2 != 0) {
+            return i;
+         }
+         
+      }
+      return -1;
+   }
+  
+   public static void printArr(String[] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         System.out.print(arr[i] + ",");
       }
       System.out.println();
-      System.out.println("The array size is " + arrList.size());
+      System.out.println("The array size is " + arr.length);
    }
 
-}
+ }
